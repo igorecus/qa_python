22b7a576256b03ccd6ae4d34d61a656b899d1f7d
igorecus <102541603+igorecus@users.noreply.github.com>
2025-02-14 22:11:16 +0500

    Delete conftest.py

--- conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from main import BooksCollector  # Импортируем класс из main.py
-
-@pytest.fixture
-def collector():
-    """Создаёт и возвращает новый экземпляр BooksCollector перед каждым тестом."""
-    return BooksCollector()
